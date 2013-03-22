6b01a336a4473b521a655489cc427489c28c7431
dougli1sqrd <dougli1sqrd@gmail.com>
2013-03-22 01:31:41 -0700

    added a 3D vector specifically

---
+++ Mathematics/src/main/java/org/edouglass/mathematics/vector/Vector3D.java
@@ -0,0 +1,14 @@
+package org.edouglass.mathematics.vector;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dougli1sqrd
+ * Date: 3/22/13
+ * Time: 1:29 AM
+ */
+public class Vector3D extends Vector    {
+
+    public Vector3D(double x, double y, double z)   {
+        super(x, y, z);
+    }
+}
